5558262e94855230b4b6ce6f74d741709515d068
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-15 16:09:06 +0500

    complete html and css for expense tracker

---
+++ app.js
@@ -0,0 +1,18 @@
+const expenseForm = document.querySelector('.expense-form');
+
+const totalBalance = document.querySelector('.balance-amt');
+const income = document.querySelector('.income-amt');
+const expenses = document.querySelector('.expenses-amt');
+
+const transList = document.querySelector('#transactionList');
+
+const expenseCategory = document.querySelector('.expense-category')
+const titleInput = document.querySelector('.new-title');
+const amountInput = document.querySelector('.new-amt');
+const selectDate = document.querySelector('.date');
+const transType = document.querySelectorAll('.transaction-type');
+
+const saveBtn = document.querySelector('.btn.full');
+
+
+
