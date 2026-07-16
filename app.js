0d3768dac3ac7c35451437c204e18141be43311d
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-16 08:50:25 +0500

    add transaction history

--- app.js
+++ app.js
@@ -12,7 +12,56 @@ const amountInput = document.querySelector('.new-amt');
 const selectDate = document.querySelector('.date');
 const transType = document.querySelectorAll('.transaction-type');
 
-const saveBtn = document.querySelector('.btn.full');
 
 
 
+expenseForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    console.log("working")
+const titleValue = titleInput.value.trim() ;
+const amountValue = Number(amountInput.value) ;
+const expenseCategoryValue = expenseCategory.value ;
+const dateValue = selectDate.value;
+
+let transTypeInput;
+
+if(transType[0].checked){
+    transTypeInput = transType[0].value;
+}else if(transType[1].checked){
+    transTypeInput = transType[1].value;
+}
+
+
+if(
+    !titleValue ||
+    !amountValue ||
+    !expenseCategoryValue ||
+    !dateValue ||
+    !transTypeInput
+){
+    alert("please fill all fields");
+    return;
+}
+
+const amountClass = transTypeInput === "income" ? "plus" : "minus";
+const sign = transTypeInput === "income" ? "+" : "-"
+
+const historyBox = document.createElement('div');
+    historyBox.classList.add('transaction');
+
+    historyBox.innerHTML = `
+    <div>
+    <h4>${titleValue}</h4>
+    <p>${expenseCategoryValue} • ${dateValue}</p>
+    </div>
+
+    <span class = "${amountClass}">
+    ${sign} PKR ${amountValue}
+    </span>
+    `;
+
+    transList.appendChild(historyBox);
+
+    expenseForm.reset();
+});
