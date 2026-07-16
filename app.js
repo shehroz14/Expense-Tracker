baf5f9debd216645e06dbfb769a56332a4397a19
Shehroz Basharat <shehrozbasharat7@gmail.com>
2026-07-16 15:25:32 +0500

    add local storage to expense-tracker

--- app.js
+++ app.js
@@ -1,56 +1,63 @@
-const expenseForm = document.querySelector('.expense-form');
+const expenseForm = document.querySelector(".expense-form");
 
-const totalBalance = document.querySelector('.balance-amt');
-const income = document.querySelector('.income-amt');
-const expenses = document.querySelector('.expenses-amt');
+const totalBalance = document.querySelector(".balance-amt");
+const income = document.querySelector(".income-amt");
+const expenses = document.querySelector(".expenses-amt");
 
-const transList = document.querySelector('#transactionList');
+const transList = document.querySelector("#transactionList");
 
-const expenseCategory = document.querySelector('.expense-category')
-const titleInput = document.querySelector('.new-title');
-const amountInput = document.querySelector('.new-amt');
-const selectDate = document.querySelector('.date');
-const transType = document.querySelectorAll('.transaction-type');
-
-
+const expenseCategory = document.querySelector(".expense-category");
+const titleInput = document.querySelector(".new-title");
+const amountInput = document.querySelector(".new-amt");
+const selectDate = document.querySelector(".date");
+const transType = document.querySelectorAll(".transaction-type");
 
 
-expenseForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
+let totalIncome = 0;
+let totalExpense = 0;
 
-    console.log("working")
-const titleValue = titleInput.value.trim() ;
-const amountValue = Number(amountInput.value) ;
-const expenseCategoryValue = expenseCategory.value ;
-const dateValue = selectDate.value;
+let transactions = []
 
-let transTypeInput;
 
-if(transType[0].checked){
+expenseForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const titleValue = titleInput.value.trim();
+  const amountValue = Number(amountInput.value);
+  const expenseCategoryValue = expenseCategory.value;
+  const dateValue = selectDate.value;
+
+  let transTypeInput;
+
+  if (transType[0].checked) {
     transTypeInput = transType[0].value;
-}else if(transType[1].checked){
+  } else if (transType[1].checked) {
     transTypeInput = transType[1].value;
-}
+  }
 
-
-if(
+  if (
     !titleValue ||
     !amountValue ||
     !expenseCategoryValue ||
     !dateValue ||
     !transTypeInput
-){
+  ) {
     alert("please fill all fields");
     return;
-}
+  }
 
-const amountClass = transTypeInput === "income" ? "plus" : "minus";
-const sign = transTypeInput === "income" ? "+" : "-"
+  if (amountValue <= 0 ){
+    alert("maount should be greater than 0");
+    return;
+  }
 
-const historyBox = document.createElement('div');
-    historyBox.classList.add('transaction');
+  const amountClass = transTypeInput === "income" ? "plus" : "minus";
+  const sign = transTypeInput === "income" ? "+" : "-";
 
-    historyBox.innerHTML = `
+  const historyBox = document.createElement("div");
+  historyBox.classList.add("transaction");
+
+  historyBox.innerHTML = `
     <div>
     <h4>${titleValue}</h4>
     <p>${expenseCategoryValue} • ${dateValue}</p>
@@ -61,7 +68,37 @@ const historyBox = document.createElement('div');
     </span>
     `;
 
-    transList.appendChild(historyBox);
+  transList.appendChild(historyBox);
 
-    expenseForm.reset();
+  if (transTypeInput === "income") {
+    totalIncome += amountValue;
+  } else {
+    totalExpense += amountValue;
+  }
+
+
+let balance = totalIncome-totalExpense
+
+  income.textContent = `PKR ${totalIncome}`;
+  expenses.textContent = `PKR ${totalExpense}`;
+  totalBalance.textContent = `PKR ${balance}`
+
+
+  let storeValue = {
+    title: titleValue,
+    amount: amountValue,
+    category: expenseCategoryValue,
+    date: dateValue,
+    transtype: transTypeInput,
+  }
+
+  transactions.push(storeValue);
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+
+
+  expenseForm.reset();
 });
+
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"))
